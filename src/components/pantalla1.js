4f42c47f927cb2b3341d41a93055825753855236
jesner98 <jesnerromero236@gmail.com>
2019-04-10 10:17:50 -0600

    esta es la pantalla principal

--- src/components/pantalla1.js
+++ src/components/pantalla1.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import{
+import {
     View,
     Text,
     ScrollView,
@@ -7,87 +7,137 @@ import{
     FlatList,
     Image,
     TouchableOpacity,
-} from 'react-native'
-import Slide from './Slider'
-   
-const Inicio =(props)=>{
-    const { categorias, productos } = props 
+    ImageBackground,
+    Button,
+} from 'react-native';
+import Slide from './Slider';
+import X from './shearch';
 
-    console.log("InicioComponente", productos)
+const Inicio = (props) => {
 
- return( 
-   
-   <ScrollView >
-        <View style={styles.listCategoria}>
+    const { categorias,
+        productos,
+        onPressItemDetalleProducto,
+        onChangeSearch,
+        searchText,
+        onPressCategoria,
+    } = props
 
-            <Text style={styles.titleCategoria}>CATEGORIAS</Text>
+    console.log("Productos de Jesner", productos);
+    
+    return (
+        <View>
 
-            <FlatList
-                data={
-                    categorias
-                }
-                renderItem={
-                    ({ item }) => (
-                        <TouchableOpacity>
-                            <View style={styles.itemDeCategoria}><Text style={styles.categoria}>{item.categoria}</Text></View>
-                        </TouchableOpacity>
-                    )
-                }
-                horizontal={true}
-                ItemSeparatorComponent={
-                    () => (
-                        <View style={styles.separator} />
-                    )
-                }
-            />
-        
-        </View>
-        <View style={[styles.promo]}> 
-            <Slide> </Slide>
-        </View>
-        <ScrollView>
-        <FlatList
-            style={styles.listaProductos}
-            data={productos}
-            renderItem={
-                ({ item }) => (
-                    <ItemProducto
-                        item={item}
+            <ImageBackground style={styles.fondo2} source={require('./../image/fondo2.png')}> 
+                <ScrollView>
+                    
+                    <X
+                        onChangeSearch={onChangeSearch}
+                        searchText={searchText}
                     />
-                )
-            }
-            horizontal={false}
-        />
-        </ScrollView>
-    </ScrollView>
-       
-)};
+                        
+                    <View style={styles.listCategoria}>
+
+                        <Text style={styles.titleCategoria}>CATEGORIAS</Text>
+
+                        <FlatList
+                            data={
+                                categorias
+                            }
+                            renderItem={
+                                ({ item }) => (
+                                    <TouchableOpacity onPress={
+                                        () => {
+                                            onPressCategoria(item);
+                                        }
+                                    }>
+                                        <View style={styles.itemDeCategoria}><Text style={styles.categoria}>{item.categoria}</Text></View>
+                                    </TouchableOpacity>
+                                )
+                            }
+                            horizontal={true}
+                            ItemSeparatorComponent={
+                                () => (
+                                    <View style={styles.separator} />
+                                )
+                            }
+                        />
+                    
+                    </View>
+                    <View style={[styles.promo]}> 
+                        <Slide> </Slide>
+                    </View>
+                    
+                    <ScrollView>
+                    <FlatList
+                        ListHeaderComponent={
+                            () => {
+                                return(
+                                    <View>
+                                        <Text style={styles.titleCategoria}>LO MAS NUEVO</Text>
+                                    </View>
+                                )
+                            }
+                        }
+                        style={styles.listaProductos}
+                        data={productos}
+                        renderItem={
+                            ({ item }) => (
+                                <ItemProducto
+                                    item={item}
+                                    onPressItemDetalleProducto={onPressItemDetalleProducto}
+                                />
+                            )
+                        }
+                        horizontal={false}
+                    />
+                    </ScrollView>
+                    
+
+                </ScrollView>
+            </ImageBackground>
+        </View>
+    )
+};
 
     const ItemProducto = (props) => {
-
-        const { item } = props
+        
+        const { item, onPressItemDetalleProducto } = props;
 
         return (
-            <View key={item.key} style={styles.header}>
-                <Image
-                    source={{uri: item.imagenUrl}}
-                    style={styles.headerImage}
-                    width={150}
-                    height={198}
-                />
-            
-                <Text style={styles.nombre}>{item.nombre}</Text>
-                <Text style={styles.descripcion}>{item.descripcion}</Text>
-                <Text style={styles.precio}> C$ {item.precio}  ¡Comprar Ahora!</Text>
-             
-             </View>
+            <TouchableOpacity
+                onPress={() => { onPressItemDetalleProducto(item.key) }}
+            >
+                <View key={item.key} style={styles.header} >
+                    <Image
+                        source={{uri: item.imagenUrl}}
+                        style={styles.headerImage}
+                        width={150}
+                        height={198}
+                    />
+                
+                    <Text style={styles.nombre}>{item.nombre}</Text>
+                    <Text style={styles.descripcion}>{item.descripcion}</Text>
+                    <Text style={styles.precio}> C$ {item.precio} </Text> 
+                </View>
+                <View style={styles.compra}>
+                    <Text>
+                        ¡Comprar Ahora!
+                    </Text>
+                </View>
+            </TouchableOpacity>
         )
 
     }
+  
 
     const styles = StyleSheet.create({
-        listaProductos: {
+        compra: {
             flex: 1,
+            width: '100%',
+            alignItems: 'flex-end',
+        },
+        listaProductos: {
             width: '100%',
             height: '100%',
         },
@@ -110,9 +160,9 @@ const Inicio =(props)=>{
     nombre:{
         color:'black',
         marginLeft:150,
-        marginRight: 0,
+        marginRight: 20,
         marginTop:-200,
-        fontSize: 20,
+        fontSize: 18,
         fontFamily: 'Fontastique',
         fontWeight: 'bold',
         fontStyle: 'italic'
@@ -178,7 +228,20 @@ const Inicio =(props)=>{
         backgroundColor:'#E5E8E8' , 
         flex: 1,
     },
+    busca:{
+        color:'blue',
+        borderColor: 'red',
+    },
     
+    Botones:{
+        width: 100
+    },
+    fondo2: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+    },
+   
       
 });
 export default Inicio;
