3d4dc96bb683b91b5316e2700664e9f828c351be
jesner98 <jesnerromero236@gmail.com>
2019-04-10 05:20:40 -0600

    creando y modificando informacion de la tienda

--- src/components/informacion.js
+++ src/components/informacion.js
@@ -5,11 +5,52 @@ import {
     Text,
 } from 'react-native'
 
-const Informacion = (props) => {
-    const { nombres, apellidos } = props
+const Informacion = () => {
     return (
         <View>
-            <Text>{nombres} {apellidos}</Text>
+             
+             <Text style={{fontWeight: 'bold',color:'black',fontSize: 16,marginRight:0,marginLeft:0,} }>
+             Deluxe nica es una tienda online. Donde el usuario la 
+             puede usar de una manera fácil y así mismo podrá encontrar
+             una variedad de ropa del mercado nacional e internacional,
+             además se le brindará una manera fácil de comprar y segura.
+            </Text>
+
+            <Text>Mision</Text>
+
+            <Text style={{fontWeight: 'bold',color:'black',fontSize: 16,marginRight:0,marginLeft:0,} }>
+             Ofrecer productos de calidad y aun precio razonable. 
+             A la misma vez mantener el interés de los Afiliados y las 
+             ganancias del negocio. También queremos expandirnos a toda 
+             Nicaragua, aumentando el número de clientes y mantenernos 
+             mejorando nuestra ventaja competitiva.
+            </Text>
+
+            <Text>Vision</Text>   
+            
+            <Text style={{fontWeight: 'bold',color:'black',fontSize: 16,marginRight:0,marginLeft:0,} }>
+             Ser la compañía enfocada en los clientes, un lugar donde la 
+             gente pueda buscar y comprar lo que anda buscando.
+            </Text>  
+
+            <Text>Valores que nos caracterizan</Text>   
+            
+            <Text style={{fontWeight: 'bold',color:'black',fontSize: 16,marginRight:0,marginLeft:0,} }>
+            •	Integridad
+            •	Responsabilidad  
+            •	Compromiso
+            •	Transparencia
+            •	Calidad
+            </Text>  
+
+            <Text>compromiso </Text>   
+            
+            <Text style={{fontWeight: 'bold',color:'black',fontSize: 16,marginRight:0,marginLeft:0,} }>
+             La tienda Deluxe Nica está comprometida a brindarle un buen servicio para que ustedes
+              se sientan muy cómodo y con la suficiente  confianza y seguridad a la hora de realizar 
+             sus transacciones.
+            </Text>  
+
         </View>
     )
 }
